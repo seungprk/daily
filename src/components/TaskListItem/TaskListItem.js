67426e90fde117a44f2b058d3dc569a03fdda41b
seungprk@gmail.com <seungprk@gmail.com>
2018-09-02 19:12:25 -0700

    Strip state from task list item component

--- src/components/TaskListItem/TaskListItem.js
+++ src/components/TaskListItem/TaskListItem.js
@@ -6,48 +6,13 @@ import ItemRepeat from './ItemRepeat';
 import ItemList from './ItemList';
 import './TaskListItem.css';
 
-class TaskListItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      typeData: null,
-    };
-    this.toggleEdit = this.toggleEdit.bind(this);
-    this.updateTypeData = this.updateTypeData.bind(this);
-  }
-
-  updateTypeData(e) {
-    const { typeData } = this.state;
-    this.setState({
-      typeData: {
-        ...typeData,
-        [e.target.id]: e.target.value,
-      },
-    });
-  }
-
-  toggleEdit() {
-    const { task, toggleEdit } = this.props;
-    const { typeData } = this.state;
-    toggleEdit(task, typeData);
-  }
-
-  render() {
-    const { task } = this.props;
-    const { typeData } = this.state;
-    return (
-      <div>
-        {task.isEdit ? (
-          <ItemEdit {...this.props} toggleEdit={this.toggleEdit} />
-        ) : (
-          <ItemDisplay {...this.props} />
-        )}
-        {task.type.name === 'repeat' ? <ItemRepeat task={task} typeData={typeData} updateTypeData={this.updateTypeData} /> : null}
-        {task.type.name === 'list' ? <ItemList task={task} typeData={typeData} updateTypeData={this.updateTypeData} /> : null}
-      </div>
-    );
-  }
-}
+const TaskListItem = ({ task }) => (
+  <div>
+    {task.isEdit ? <ItemEdit /> : <ItemDisplay />}
+    {task.type.name === 'repeat' ? <ItemRepeat /> : null}
+    {task.type.name === 'list' ? <ItemList /> : null}
+  </div>
+);
 
 TaskListItem.propTypes = {
   task: PropTypes.shape({
@@ -58,9 +23,6 @@ TaskListItem.propTypes = {
       name: PropTypes.string.isRequired,
     }).isRequired,
   }).isRequired,
-  toggleTask: PropTypes.func.isRequired,
-  toggleEdit: PropTypes.func.isRequired,
-  deleteTask: PropTypes.func.isRequired,
 };
 
 export default TaskListItem;
