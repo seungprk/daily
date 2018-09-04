ec20b416adf83eaac812ef4ec9eeef36d0c49513
seungprk@gmail.com <seungprk@gmail.com>
2018-09-03 17:46:00 -0700

    Clear sub list item input field when added

--- src/components/ItemList/index.js
+++ src/components/ItemList/index.js
@@ -6,14 +6,22 @@ class ItemList extends React.Component {
     super(props);
     this.state = { value: '' };
     this.handleChange = this.handleChange.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
   }
 
   handleChange(e) {
     this.setState({ value: e.target.value });
   }
 
+  handleAdd() {
+    const { addItem } = this.props;
+    const { value } = this.state;
+    addItem(value);
+    this.setState({ value: '' });
+  }
+
   render() {
-    const { task, addItem } = this.props;
+    const { task } = this.props;
     const { value } = this.state;
     const { data } = task.type;
     return (
@@ -21,7 +29,7 @@ class ItemList extends React.Component {
         {data ? data.map(item => <SubListItem {...item} isEdit={task.isEdit} />) : null}
         <div>
           <input type="text" value={value} onChange={this.handleChange} />
-          <button type="button" onClick={() => addItem(value)}>
+          <button type="button" onClick={this.handleAdd}>
             Add New Item
           </button>
         </div>
